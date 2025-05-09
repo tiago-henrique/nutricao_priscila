d002bbeeede23a91b39295e2d3610eef9c337a9a
Tiago Henrique <tiago_henrique@hotmail.com>
2025-05-09 08:14:52 -0300

    Update primeira_2.py

--- primeira_2.py
+++ primeira_2.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 st.set_page_config(layout='wide')
-url = ("https://lmu.famerp.br/tiago/indicadores.csv")
+url = ("https://lmu.famerp.br/tiago/indicadores_2.csv")
 df = pd.read_csv(url)
 
 pc = df['data'].count()
